66f2b2ac292891b3d22a9fdd062c25528151b2a6
cristibc <borcan.cristian1@gmail.com>
2024-01-08 14:34:52 +0200

    added secrets.json

--- Greenscape/Project1/Program.cs
+++ Greenscape/Project1/Program.cs
@@ -1,16 +1,18 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Project1.Data;
+using Project1.Model;
 using Project1.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var secretConfiguration = new ConfigurationBuilder()
+    .AddJsonFile("secrets.json")
+    .Build();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(
-            builder.Configuration.GetConnectionString("DefaultConnection")));
+        options.UseSqlServer(secretConfiguration["DefaultConnection"]));
 builder.Services.AddDefaultIdentity<IdentityUser>
     (options =>
     {
@@ -29,12 +31,10 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<IEmailSender>(provider =>
 {
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    var apiKey = configuration["SendGrid:ApiKey"];
-    return new SendGridEmailSender(apiKey);
+    /*var configuration = provider.GetRequiredService<IConfiguration>();
+    var apiKey = configuration["SendGrid:ApiKey"];*/
+    return new SendGridEmailSender(secretConfiguration["SendGrid_APIKey"]);
 });
-
-
 
 var app = builder.Build();
 
